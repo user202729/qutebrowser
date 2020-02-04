cb529183180a1915d3f9aa1a4df82bde8d058359
user202729 <25191436+user202729@users.noreply.github.com>
2020-02-04 07:52:24 +0700

    Allow scrollable nav

--- qutebrowser/javascript/webelem.js
+++ qutebrowser/javascript/webelem.js
@@ -240,7 +240,7 @@ window._qutebrowser.webelem = (function() {
                 document.scrollingElement, null, "scrollable"));
         }
         scrollable_elts = scrollable_elts.concat(
-            Array.from(document.querySelectorAll("div,ol,ul")).
+            Array.from(document.querySelectorAll("div,ol,ul,nav")).
                 filter((elt) => elt.clientHeight < elt.scrollHeight &&
                         scroll.is_scrollable_y(elt)).
                 filter((elt) => !only_visible || is_visible(elt)).
