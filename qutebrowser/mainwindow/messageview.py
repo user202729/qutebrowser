4a47e34021e79e6cdd2157728955f63e497d0af5
user202729 <25191436+user202729@users.noreply.github.com>
2020-04-07 12:24:52 +0700

    Fix JS confirm/prompt wrapping

--- qutebrowser/mainwindow/messageview.py
+++ qutebrowser/mainwindow/messageview.py
@@ -36,7 +36,6 @@ class Message(QLabel):
         super().__init__(text, parent)
         self.replace = replace
         self.setAttribute(Qt.WA_StyledBackground, True)
-        self.setWordWrap(True)
         qss = """
             padding-top: 2px;
             padding-bottom: 2px;
